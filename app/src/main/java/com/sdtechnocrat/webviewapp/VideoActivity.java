42fe69e17c260fc5fed2ebc2bc4a5624a65b09a2
Satyaranjan Dalai <satyaranjan@muvi.com>
2023-04-17 21:49:15 +0530

    - Removed Unused Assets - Added Smart Khana flavor - Refactoring

--- app/src/main/java/com/sdtechnocrat/webviewapp/VideoActivity.java
+++ app/src/main/java/com/sdtechnocrat/webviewapp/VideoActivity.java
@@ -1,19 +1,21 @@
 package com.sdtechnocrat.webviewapp;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class VideoActivity extends AppCompatActivity {
 
@@ -26,12 +28,12 @@ public class VideoActivity extends AppCompatActivity {
 
         mWebView = findViewById(R.id.webView);
 
-        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.setWebViewClient(new MoonTVWebViewClient());   //TODO if
         mWebView.setWebChromeClient(new VideoChromeClient());
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setAllowFileAccess(true);
-        webSettings.setAppCacheEnabled(true);
+        //webSettings.setAppCacheEnabled(true);
 
         if (savedInstanceState == null) {
             //mWebView.loadUrl("https://www.youtube.com/");
@@ -77,6 +79,24 @@ public class VideoActivity extends AppCompatActivity {
             this.mCustomViewCallback = paramCustomViewCallback;
             ((FrameLayout)getWindow().getDecorView()).addView(this.mCustomView, new FrameLayout.LayoutParams(-1, -1));
             getWindow().getDecorView().setSystemUiVisibility(3846 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        }
+    }
+
+    private class MoonTVWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            //return super.shouldOverrideUrlLoading(view, request);
+            if (request.getUrl().toString().contains("whatsapp://send")
+                    /*|| request.getUrl().toString().contains("fb://facewebmodal")*/
+            ) {
+
+                //the link is not for a page on my site, so launch another Activity that handles URLs
+                Intent intent = new Intent(Intent.ACTION_VIEW, request.getUrl());
+                startActivity(intent);
+                return true;
+            }
+            // This is my website, so do not override; let my WebView load the page
+            return false;
         }
     }
 
